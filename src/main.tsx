2a32055720bc832e127bb6a980b32b306c64798c
Tomz Dang <dangjingtao@163.com>
2026-09-10 15:22:17 +0800

    feat: mount GitHub governance view

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import Governance from "./Governance";
 import "./styles.css";
 import "./ops.css";
+import "./governance.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <>
+      <App />
+      <Governance />
+    </>
   </StrictMode>,
 );
