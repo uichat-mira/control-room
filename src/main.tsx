06b8b6492020f0967ea2be18f00ed08dce2d1099
Tomz Dang <dangjingtao@163.com>
2026-09-10 15:49:40 +0800

    ui: keep dashboard hierarchy inside the main shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import Governance from "./Governance";
 import "./styles.css";
 import "./ops.css";
 import "./governance.css";
+import "./visual.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
-      <App />
-      <Governance />
-    </>
+    <App />
   </StrictMode>,
 );
